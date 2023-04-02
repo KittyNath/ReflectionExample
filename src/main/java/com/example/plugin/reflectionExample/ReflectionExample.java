b2f4eec616f92f8e314da41ab0ebc1978fe4f66e
Nathalie <kitty.kitty.nath@gmail.com>
2023-04-02 18:31:57 +0200

    Upload

---
+++ src/main/java/com/example/plugin/reflectionExample/ReflectionExample.java
@@ -0,0 +1,23 @@
+package com.example.plugin.reflectionExample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Method;
+
+public class ReflectionExample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        try {
+            Class<?> fmlCommonHandlerClass = Class.forName("net.minecraftforge.fml.common.FMLCommonHandler");
+            Class<?> minecraftServerClass = Class.forName("net.minecraft.server.MinecraftServer");
+            Class<?> playerListClass = Class.forName("net.minecraft.server.management.PlayerList");
+
+            Method getMinecraftServerInstanceMethod = fmlCommonHandlerClass.getDeclaredMethod("getMinecraftServerInstance");
+            Method getPlayerListMethod = minecraftServerClass.getDeclaredMethod("getPlayerList");
+            Method getPlayersMethod = playerListClass.getMethod("func_181057_v");
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
